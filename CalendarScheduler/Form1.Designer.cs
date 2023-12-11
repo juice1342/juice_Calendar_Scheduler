0304eee36b0dd0ba5afb746e68ed90aa432142d3
Lamool <gosu0522@naver.com>
2023-12-12 01:37:54 +0900

    일정출력 기능 추가

--- CalendarScheduler/Form1.Designer.cs
+++ CalendarScheduler/Form1.Designer.cs
@@ -35,17 +35,17 @@
             this.txtAddToDoList = new System.Windows.Forms.TextBox();
             this.btnAddToDoList = new System.Windows.Forms.Button();
             this.btnDelToDoList = new System.Windows.Forms.Button();
-            this.listViewToDoList = new System.Windows.Forms.ListView();
-            this.columnSchedule = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lblTimeDate = new System.Windows.Forms.Label();
             this.lblTimeClock = new System.Windows.Forms.Label();
             this.TimerClock = new System.Windows.Forms.Timer(this.components);
+            this.columnDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnSchedule = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listViewToDoList = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(26, 122);
-            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(13, 14, 13, 14);
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 81);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
             this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected_1);
@@ -54,10 +54,9 @@
             // 
             this.lblTodoList.AutoSize = true;
             this.lblTodoList.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lblTodoList.Location = new System.Drawing.Point(357, 122);
-            this.lblTodoList.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTodoList.Location = new System.Drawing.Point(250, 81);
             this.lblTodoList.Name = "lblTodoList";
-            this.lblTodoList.Size = new System.Drawing.Size(103, 20);
+            this.lblTodoList.Size = new System.Drawing.Size(67, 13);
             this.lblTodoList.TabIndex = 1;
             this.lblTodoList.Text = "오늘 할 일";
             // 
@@ -65,28 +64,25 @@
             // 
             this.lblAddToDoList.AutoSize = true;
             this.lblAddToDoList.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lblAddToDoList.Location = new System.Drawing.Point(21, 392);
-            this.lblAddToDoList.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAddToDoList.Location = new System.Drawing.Point(15, 261);
             this.lblAddToDoList.Name = "lblAddToDoList";
-            this.lblAddToDoList.Size = new System.Drawing.Size(96, 20);
+            this.lblAddToDoList.Size = new System.Drawing.Size(63, 13);
             this.lblAddToDoList.TabIndex = 3;
             this.lblAddToDoList.Text = "일정 추가";
             // 
             // txtAddToDoList
             // 
-            this.txtAddToDoList.Location = new System.Drawing.Point(26, 418);
-            this.txtAddToDoList.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAddToDoList.Location = new System.Drawing.Point(18, 279);
             this.txtAddToDoList.Multiline = true;
             this.txtAddToDoList.Name = "txtAddToDoList";
-            this.txtAddToDoList.Size = new System.Drawing.Size(753, 60);
+            this.txtAddToDoList.Size = new System.Drawing.Size(528, 41);
             this.txtAddToDoList.TabIndex = 4;
             // 
             // btnAddToDoList
             // 
-            this.btnAddToDoList.Location = new System.Drawing.Point(691, 489);
-            this.btnAddToDoList.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddToDoList.Location = new System.Drawing.Point(484, 326);
             this.btnAddToDoList.Name = "btnAddToDoList";
-            this.btnAddToDoList.Size = new System.Drawing.Size(89, 34);
+            this.btnAddToDoList.Size = new System.Drawing.Size(62, 23);
             this.btnAddToDoList.TabIndex = 5;
             this.btnAddToDoList.Text = "추가";
             this.btnAddToDoList.UseVisualStyleBackColor = true;
@@ -94,41 +90,21 @@
             // 
             // btnDelToDoList
             // 
-            this.btnDelToDoList.Location = new System.Drawing.Point(691, 330);
-            this.btnDelToDoList.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelToDoList.Location = new System.Drawing.Point(484, 220);
             this.btnDelToDoList.Name = "btnDelToDoList";
-            this.btnDelToDoList.Size = new System.Drawing.Size(89, 34);
+            this.btnDelToDoList.Size = new System.Drawing.Size(62, 23);
             this.btnDelToDoList.TabIndex = 6;
             this.btnDelToDoList.Text = "삭제";
             this.btnDelToDoList.UseVisualStyleBackColor = true;
             this.btnDelToDoList.Click += new System.EventHandler(this.btnDelToDoList_Click_1);
             // 
-            // listViewToDoList
-            // 
-            this.listViewToDoList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnSchedule});
-            this.listViewToDoList.HideSelection = false;
-            this.listViewToDoList.Location = new System.Drawing.Point(357, 150);
-            this.listViewToDoList.Margin = new System.Windows.Forms.Padding(4);
-            this.listViewToDoList.Name = "listViewToDoList";
-            this.listViewToDoList.OwnerDraw = true;
-            this.listViewToDoList.Size = new System.Drawing.Size(421, 169);
-            this.listViewToDoList.TabIndex = 8;
-            this.listViewToDoList.UseCompatibleStateImageBehavior = false;
-            // 
-            // columnSchedule
-            // 
-            this.columnSchedule.Text = "일정";
-            this.columnSchedule.Width = 120;
-            // 
             // lblTimeDate
             // 
             this.lblTimeDate.AutoSize = true;
             this.lblTimeDate.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lblTimeDate.Location = new System.Drawing.Point(22, 71);
-            this.lblTimeDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTimeDate.Location = new System.Drawing.Point(15, 47);
             this.lblTimeDate.Name = "lblTimeDate";
-            this.lblTimeDate.Size = new System.Drawing.Size(109, 23);
+            this.lblTimeDate.Size = new System.Drawing.Size(72, 15);
             this.lblTimeDate.TabIndex = 9;
             this.lblTimeDate.Text = "현재 날짜";
             // 
@@ -136,10 +112,9 @@
             // 
             this.lblTimeClock.AutoSize = true;
             this.lblTimeClock.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lblTimeClock.Location = new System.Drawing.Point(21, 20);
-            this.lblTimeClock.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTimeClock.Location = new System.Drawing.Point(15, 13);
             this.lblTimeClock.Name = "lblTimeClock";
-            this.lblTimeClock.Size = new System.Drawing.Size(195, 40);
+            this.lblTimeClock.Size = new System.Drawing.Size(134, 27);
             this.lblTimeClock.TabIndex = 10;
             this.lblTimeClock.Text = "현재 시각";
             // 
@@ -147,11 +122,38 @@
             // 
             this.TimerClock.Interval = 1000;
             // 
+            // columnDate
+            // 
+            this.columnDate.Text = "날짜";
+            this.columnDate.Width = 100;
+            // 
+            // columnSchedule
+            // 
+            this.columnSchedule.Text = "일정";
+            this.columnSchedule.Width = 180;
+            // 
+            // listViewToDoList
+            // 
+            this.listViewToDoList.BackColor = System.Drawing.SystemColors.Window;
+            this.listViewToDoList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnDate,
+            this.columnSchedule});
+            this.listViewToDoList.FullRowSelect = true;
+            this.listViewToDoList.GridLines = true;
+            this.listViewToDoList.HideSelection = false;
+            this.listViewToDoList.Location = new System.Drawing.Point(250, 100);
+            this.listViewToDoList.Name = "listViewToDoList";
+            this.listViewToDoList.OwnerDraw = true;
+            this.listViewToDoList.Size = new System.Drawing.Size(296, 114);
+            this.listViewToDoList.TabIndex = 8;
+            this.listViewToDoList.UseCompatibleStateImageBehavior = false;
+            this.listViewToDoList.View = System.Windows.Forms.View.Details;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 608);
+            this.ClientSize = new System.Drawing.Size(568, 405);
             this.Controls.Add(this.lblTimeClock);
             this.Controls.Add(this.lblTimeDate);
             this.Controls.Add(this.listViewToDoList);
@@ -161,7 +163,7 @@
             this.Controls.Add(this.lblAddToDoList);
             this.Controls.Add(this.lblTodoList);
             this.Controls.Add(this.monthCalendar1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -179,11 +181,12 @@
         private System.Windows.Forms.TextBox txtAddToDoList;
         private System.Windows.Forms.Button btnAddToDoList;
         private System.Windows.Forms.Button btnDelToDoList;
-        private System.Windows.Forms.ListView listViewToDoList;
-        private System.Windows.Forms.ColumnHeader columnSchedule;
         private System.Windows.Forms.Label lblTimeDate;
         private System.Windows.Forms.Label lblTimeClock;
         private System.Windows.Forms.Timer TimerClock;
+        private System.Windows.Forms.ColumnHeader columnDate;
+        private System.Windows.Forms.ColumnHeader columnSchedule;
+        private System.Windows.Forms.ListView listViewToDoList;
     }
 }
 
--- CalendarScheduler/Form1.cs
+++ CalendarScheduler/Form1.cs
@@ -13,6 +13,8 @@ namespace CalendarScheduler
 {
     public partial class Form1 : Form
     {
+        //private ListView listView;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +24,14 @@ namespace CalendarScheduler
             TimerClock.Start();
 
             CurrentDateClock();
+
+            // ListView 초기화
+            InitializeListView();
+
+            // 필요에 따라 폼이 초기화될 때 기존 일정을 ListView에 로드
+            LoadExistingSchedules();
         }
+
         private void TimerClock_Tick(object sender, EventArgs e)
         {
             CurrentDateClock();
@@ -57,6 +66,7 @@ namespace CalendarScheduler
             // 파일 중복 화인, 중복일 경우 파일이름(숫자).txt로 저장됨
             bool fileExist = true;
             int fileCnt = 0;
+
             while (fileExist)
             {
                 if (File.Exists(filePath_str))
@@ -70,17 +80,42 @@ namespace CalendarScheduler
                     fileExist = false;
                 }
             }
+
             StreamWriter writer;
             writer = File.CreateText(filePath_str);
             writer.Write(txtAddToDoList.Text);
             writer.Close();
             MessageBox.Show("일정이 저장되었습니다!");
+
+
+            // ListView에 일정 정보 추가
+            AddScheduleToListView(GetSelectedDate(), txtAddToDoList.Text);
         }
 
         private void monthCalendar1_DateSelected_1(object sender, DateRangeEventArgs e)
         {
             DateTime selectedDate = monthCalendar1.SelectionRange.Start;
             SelectedDate = selectedDate.ToString("yyyy-MM-dd");
+
+
+            // 해당 날짜에 대한 일정 파일 목록 가져오기
+            List<string> scheduleFiles = GetScheduleFilesForDate(selectedDate);
+
+            // 리스트뷰 초기화
+            listViewToDoList.Items.Clear();
+
+            // 각 파일에 대한 일정을 리스트뷰에 추가
+            foreach (string file in scheduleFiles)
+            {
+                List<string> schedules = LoadSchedulesFromFile(file);
+
+                foreach (string schedule in schedules)
+                {
+                    ListViewItem item = new ListViewItem(selectedDate.ToString("yyyy-MM-dd"));
+                    item.SubItems.Add(schedule);
+                    listViewToDoList.Items.Add(item);
+                }
+            }
         }
         public string GetSelectedDate()
         {
@@ -89,6 +124,7 @@ namespace CalendarScheduler
 
         private void btnDelToDoList_Click_1(object sender, EventArgs e)
         {
+            /*
             string selectedFileName = "";   // 선택된 파일 경로
             if (File.Exists(selectedFileName) == true)
             {
@@ -98,8 +134,99 @@ namespace CalendarScheduler
             {
                 MessageBox.Show("존재하지 않는 일정입니다.");
             }
+            */
+
+            // 선택된 항목이 있는지 확인
+            if (listViewToDoList.SelectedItems.Count > 0)
+            {
+                // 선택된 항목의 날짜를 가져오기
+                string selectedDate = listViewToDoList.SelectedItems[0].Text;
+
+                // 해당 날짜의 파일 경로 생성
+                string filePathToDelete = Path.Combine(@"c:\CalendarScheduler", $"{selectedDate}.txt");
+
+                // 파일이 존재하는지 확인 후 삭제
+                if (File.Exists(filePathToDelete))
+                {
+                    File.Delete(filePathToDelete);
+
+                    // ListView에서도 선택된 항목 삭제
+                    listViewToDoList.SelectedItems[0].Remove();
+
+                    MessageBox.Show("일정이 삭제되었습니다!");
+                }
+                else
+                {
+                    MessageBox.Show("존재하지 않는 일정입니다.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("삭제할 항목을 선택하세요.");
+            }
         }
 
-        
+
+        private void InitializeListView()
+        {
+            // ListView 컨트롤 생성
+            listViewToDoList = new ListView();
+            listViewToDoList.View = View.Details;
+
+            // ListView에 열 추가
+            listViewToDoList.Columns.Add("날짜", 110);
+            listViewToDoList.Columns.Add("일정", 250);
+
+            listViewToDoList.Size = new Size(296, 114);
+            listViewToDoList.Location = new Point(250, 100);
+
+            // ListView를 폼에 추가
+            Controls.Add(listViewToDoList);
+        }
+
+        private void LoadExistingSchedules()
+        {
+            // 기존 일정을 ListView에 로드
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"c:\CalendarScheduler");
+
+            if (directoryInfo.Exists)
+            {
+                foreach (var file in directoryInfo.GetFiles("*.txt"))
+                {
+                    string date = Path.GetFileNameWithoutExtension(file.Name);
+                    string schedule = File.ReadAllText(file.FullName);
+
+                    // ListView에 일정 정보 추가
+                    AddScheduleToListView(date, schedule);
+                }
+            }
+        }
+
+        private void AddScheduleToListView(string date, string schedule)
+        {
+            // ListView에 일정 정보 추가
+            ListViewItem item = new ListViewItem(date);
+            item.SubItems.Add(schedule);
+            listViewToDoList.Items.Add(item);
+        }
+
+
+        private List<string> GetScheduleFilesForDate(DateTime selectedDate)
+{
+            string directoryPath = @"c:\CalendarScheduler\";
+
+            // 해당 날짜에 대한 일정 파일들을 검색
+            string[] allFiles = Directory.GetFiles(directoryPath, selectedDate.ToString("yyyy-MM-dd") + "*.txt");
+
+            return allFiles.ToList();
+        }
+
+        private List<string> LoadSchedulesFromFile(string filePath)
+        {
+            // TODO: 실제로는 여기에서 데이터를 가져오는 코드를 구현해야 합니다.
+            // 예제로 간단히 텍스트를 리턴하도록 하겠습니다.
+            return File.ReadAllLines(filePath).ToList();
+        }
+
     }
 }
